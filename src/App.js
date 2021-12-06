693c4842c3114d67a2617f6e3cbc22e52b4b73d3
madgelackie <marionmlackie@gmail.com>
2021-12-06 14:27:22 +0000

    xml feed data coming in to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  // const [feedURL, setFeedURL] = useState([]);
+  const [feed, setFeed] = useState([]);
+
+  const rssFeed = "https://feed.podbean.com/deltaee/feed.xml"
+
+  useEffect(() => {
+    fetch(rssFeed)
+    .then(res => res.text())
+    .then(str => {setFeed(str)})
+    }, [])
+
+
+    // .then(str => {
+    //   const parser = new window.DOMParser();
+    //   var data = parser.parseFromString(str, "text/xml");
+    //   const itemList = data.querySelectorAll('item');
+    //   const items=[];
+    //     itemList.forEach(el => {
+    //       items.push(
+    //         el.querySelector('title').innerHTML,
+    //       )
+    //     })
+    //   })
+    //   .then((items) => {setFeed(items)})
+    // }, [])
+    
+  // const titleList = feed.map((feedItem, index) => {
+  //   return <li key={index}>
+  //     {feedItem}
+  //   </li>
+  // })
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <p>Hello podcast fan</p>
+    </>
   );
 }
 

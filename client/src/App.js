6f045d6dce2bb927426a8bf3f223e9293bddbb97
madgelackie <marionmlackie@gmail.com>
2021-12-11 21:03:01 +0000

    switch between channel input and feed page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import MainContainer from './container/MainContainer';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ChannelContainer from './container/ChannelContainer';
+
 
 function App() {
 
   return (
-    <div>
-      <MainContainer />
-    </div>
+    <>
+    <Router>
+      <Switch>
+      <Route path="/feed" exact>
+        <MainContainer />
+      </Route>
+      <Route path="/channels" exact>
+        <ChannelContainer />
+      </Route>
+    </Switch>
+    </Router>
+    </>
   );
 
 }

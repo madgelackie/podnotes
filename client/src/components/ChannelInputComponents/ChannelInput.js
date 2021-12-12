f3f7d29fe3789b6bcc0db31b9ec2022ce7c274b1
madgelackie <marionmlackie@gmail.com>
2021-12-12 00:11:28 +0000

    channel container state updating with inputed url

--- client/src/components/ChannelInputComponents/ChannelInput.js
+++ client/src/components/ChannelInputComponents/ChannelInput.js
@@ -1,10 +1,35 @@
-const ChannelInput = () => {
+import { useEffect, useState } from 'react';
+
+const ChannelInput = ({onUrlSubmit}) => {
+
+
+    const [urlInput, setUrlInput] = useState(null);
+
+    const handleUrlSubmit = (event) => {
+        event.preventDefault();
+        onUrlSubmit(urlInput);
+        setUrlInput("");
+    }
+
+    const handleUrlChange = (event) => {
+        setUrlInput(event.target.value);
+    }
+
+    
 
     return (
-        <>
+        <>  
+        <form onSubmit={handleUrlSubmit}>
             <label for="rss-url">RSS Feed URL</label>
-            <input type="url" id="rss-url" name="rss-url"/><br></br>
-            <button >Save</button>
+            <input 
+            type="url" 
+            id="rss-url" 
+            value={urlInput}
+            onChange={handleUrlChange}
+            required
+            /><br></br>
+            <button id="submit" type="submit" value="Submit">Save</button>
+        </form>
         </>
     )
 
--- client/src/container/ChannelContainer.js
+++ client/src/container/ChannelContainer.js
@@ -1,11 +1,18 @@
 import ChannelInput from "../components/ChannelInputComponents/ChannelInput";
 import ChannelList from "../components/ChannelInputComponents/ChannelList";
+import { useEffect, useState } from 'react';
 
 const ChannelContainer = () => {
 
+    const [newUrl, setNewUrl] = useState([]);
+
+    const onUrlSubmit = (url) => {
+        setNewUrl(url)
+    }
+
     return (
         <>
-            <ChannelInput />
+            <ChannelInput onUrlSubmit={onUrlSubmit} />
             <br></br>
             <ChannelList />
         </>

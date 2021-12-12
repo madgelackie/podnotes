0221f6a480a541489cc03b9b9d8320f7e24a6fc5
madgelackie <marionmlackie@gmail.com>
2021-12-12 17:55:28 +0000

    working on extracting details from saved rss feed xml documents

--- client/src/container/ChannelContainer.js
+++ client/src/container/ChannelContainer.js
@@ -6,8 +6,8 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 const ChannelContainer = () => {
 
-
     const [savedFeeds, setSavedFeeds] = useState([]);
+    // const [feedDetails, setFeedDetails] = useState([]);
 
 // this function is making a request to the api, and the resposne is held in channelPromise
 // and then this is used to set the state savedFeeds 
@@ -15,11 +15,50 @@ const ChannelContainer = () => {
         const request = new Request();
         const channelPromise = request.get("/api/channels")
         .then((data) => {setSavedFeeds(data)})
-    }, []);
-
-// following function will make a network call to each of the saved RSS Feeds, to access the xml document, extract the required information and hold that in an object that we will then interact with. 
+        }, []);
 
 
+    const prepForFeedLoop = [...savedFeeds];
+    const feedDetails = [];
+    console.log(prepForFeedLoop[2].channelUrl)
+    const extractUrlTest = prepForFeedLoop.forEach(object => feedDetails.push(object.channelUrl));
+    console.log(feedDetails)
+
+
+    // const extractUrlTest = prepForFeedLoop.forEach(object => console.log(channelUrl));
+    // console.log(extractUrlTest);
+    
+
+// the following function will make a network call to each of the saved RSS Feeds, to access the xml document, extract the required information and hold that in an object that we will then interact with. 
+    // const requestAll = (savedFeeds) => {
+    //     for (const feed of savedFeeds){
+    //         fetch(feed)
+    //         .then(res => res.text())
+    //         .then(str => {
+    //         const parser = new window.DOMParser();
+    //         const data = parser.parseFromString(str, 'text/xml');
+    //         console.log(data);
+    //         const itemNodeList = data.querySelectorAll('item');
+    //         const feedURL = data.querySelector('channel');
+    //         console.log(feedURL);
+    //         const image = feedURL.getAttribute('url');
+    //         console.log(image);
+    //         console.log(itemNodeList);
+    //         const items=[];
+    //         itemNodeList.forEach(item => {
+    //         items.push({
+    //         title: item.querySelector('title').innerHTML,
+    //         mp3: item.querySelector('enclosure').getAttribute('url'),
+    //         })
+    //         })
+    //         setFeedDetails(items);
+    //     }) 
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     requestAll()
+    // }, [])
 
     const onUrlSubmit = function(feedUrl){
         const request = new Request();
@@ -43,7 +82,7 @@ const ChannelContainer = () => {
         
     )
 
-
+        
 }
 
 export default ChannelContainer;

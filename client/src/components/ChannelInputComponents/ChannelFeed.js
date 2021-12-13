7dd165c6388818986126e9fb8c1d0d429f457f6b
madgelackie <marionmlackie@gmail.com>
2021-12-13 15:24:41 +0000

    before further changes

--- client/src/components/ChannelInputComponents/ChannelFeed.js
+++ client/src/components/ChannelInputComponents/ChannelFeed.js
@@ -38,20 +38,13 @@ const ChannelFeed = ({selectedFeed}) => {
             }) 
         }, [selectedFeedUrl])
 
-
+// episode selection occurs in this (ChannelFeed) component.
     const handleEpisodeSelect = (event) => {
         const chosenEpisode = feed[event.target.value];
         setEpisodeToPlay(chosenEpisode);
-        // const request = new Request();
-        // request.post("/api/episodes", chosenEpisode)
     }
 
-    // const onUrlSubmit = function(feedUrl){
-    //     const request = new Request();
-    //     request.post("/api/channels", feedUrl)  
-    //     .then(() => window.location = "/channels")
-    // };
-
+// rendering instructions for this (ChannelFeed) component.
     const titleList = feed.map((feedItem, index) => {
         return <div id="feed-items" key={index}>
                 <li value={index}>{feedItem.episodeTitle} </li>
@@ -64,10 +57,6 @@ const ChannelFeed = ({selectedFeed}) => {
         const episodeBookmarkList = [...episodeBookmarks, bookmark];
         setEpisodeBookmarks(episodeBookmarkList)
     }       
-    // <div>
-    // {/* {episodeToPlay ? <EpisodePlayer episode={episodeToPlay} onBookmarkClicked={onBookmarkClicked}/>:null} */}
-    // {/* {episodeToPlay ? <NoteBox episodeBookmarks={episodeBookmarks}/>:null} */}
-    // </div>   
 
     return(
         <>

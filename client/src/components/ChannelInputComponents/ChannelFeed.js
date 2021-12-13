bb706899bb9bf14815e6b3ef8b6fdc95aa476f68
madgelackie <marionmlackie@gmail.com>
2021-12-13 13:45:07 +0000

    episodes playing on select

--- client/src/components/ChannelInputComponents/ChannelFeed.js
+++ client/src/components/ChannelInputComponents/ChannelFeed.js
@@ -1,10 +1,12 @@
 import {useEffect, useState } from 'react';
+import EpisodePlayer from './EpisodePlayer';
 
 
 const ChannelFeed = ({selectedFeed}) => {
 
     const [selectedFeedUrl, setSelectedFeedUrl] = useState(null);
     const [feed, setFeed] = useState([]);
+    const [episodeToPlay, setEpisodeToPlay] = useState(null);
 
     useEffect(() => {
         const urlOnly = selectedFeed.channelUrl;
@@ -32,24 +34,30 @@ const ChannelFeed = ({selectedFeed}) => {
         }, [selectedFeedUrl])
 
 
-    // const handleEpisodeSelect = (event) => {
-    //     const chosenEpisode = feed[event.target.value];
-    //     onEpisodeSelect(chosenEpisode)
-    // }
+    const handleEpisodeSelect = (event) => {
+        const chosenEpisode = feed[event.target.value];
+        setEpisodeToPlay(chosenEpisode);
+    }
 
     const titleList = feed.map((feedItem, index) => {
         return <div id="feed-items" key={index}>
                 <li value={index}>{feedItem.title} </li>
-                <button value={index}>Listen</button>
+                <button value={index} onClick={handleEpisodeSelect}>Listen</button>
         </div>
     })
 
-    
+    // <div>
+    // {/* {episodeToPlay ? <EpisodePlayer episode={episodeToPlay} onBookmarkClicked={onBookmarkClicked}/>:null} */}
+    // {/* {episodeToPlay ? <NoteBox episodeBookmarks={episodeBookmarks}/>:null} */}
+    // </div>   
+
     return(
+        <>
         <div>
             <ul>{titleList}</ul>
         </div>
-        
+        {episodeToPlay ? <EpisodePlayer episode={episodeToPlay}/>:null}
+        </>
     )
     
 

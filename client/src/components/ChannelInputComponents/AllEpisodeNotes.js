53f277f293be78327a82cf1cf2e2d219092f9c66
madgelackie <marionmlackie@gmail.com>
2021-12-15 21:45:55 +0000

    display bookmarks not working

--- client/src/components/ChannelInputComponents/AllEpisodeNotes.js
+++ client/src/components/ChannelInputComponents/AllEpisodeNotes.js
@@ -4,28 +4,36 @@ import Request from '../../services/helper';
 const AllEpisodeNotes = ({episodeDBReady, singleBookmark}) => {
 
     const [episodeFromDB, setEpisodeFromDB] = useState(null);
+    const [epBookmarks, setEpBookmarks] = useState([]);
     
     useEffect(() => {
         if (episodeDBReady){
         const request = new Request();
         request.get("/api/episodes/" + episodeDBReady.episodeTitle)
         .then((data) => {setEpisodeFromDB(data)})}
-    }, [episodeDBReady])
+    }, [])
 
     useEffect(() => {
         if (episodeDBReady){
         const request = new Request();
         request.get("/api/episodes/" + episodeDBReady.episodeTitle)
         .then((data) => {setEpisodeFromDB(data)})}
-    }, [singleBookmark])
+    }, [])
 
     console.log("What comes from ChannelFeed: ", episodeDBReady)
     console.log("This is the returned ep from DB: ", episodeFromDB)
+    // console.log("Ep title ", episodeFromDB.episodeTitle)
+
+    // const bookmarkList = episodeFromDB.map((ep, index) => {
+    //     return <div id="bookmark" key={index}>
+    //             <li value={index}>{episodeFromDB.episodeTitle}</li>
+    //     </div>
+    // })
     
 
     return (
         <div class="all-notes">
-        <p>boop</p>
+        <p> </p>
         </div>
     )
 }

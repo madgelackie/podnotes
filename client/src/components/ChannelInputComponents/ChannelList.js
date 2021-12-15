d270be079d2435abb1c75484f47ff37415c4ddf3
madgelackie <marionmlackie@gmail.com>
2021-12-15 23:13:43 +0000

    small css, bookmarks still working

--- client/src/components/ChannelInputComponents/ChannelList.js
+++ client/src/components/ChannelInputComponents/ChannelList.js
@@ -9,15 +9,7 @@ const ChannelList = ({savedFeeds}) => {
     const handleSelect = (event) => {
         const chosenFeed = savedFeeds[event.target.value];
         setSelectedFeed(chosenFeed)
-        // window.location = "/channels/" + chosenFeed.id;
     } 
-
-    // const showList = savedFeeds.map((feed, index) => {
-    //     return <div key={index}>
-    //         <li>{feed.channelUrl}</li>
-    //         <button value={index} onClick={handleSelect}>Select</button>
-    //         </div>     
-    // })
 
     const showList = savedFeeds.map((feed, index) => {
         return <option key={index} value={index}>{feed.channelUrl}</option>
@@ -26,7 +18,7 @@ const ChannelList = ({savedFeeds}) => {
 
     return (
         <>
-        <div>
+        <div id="channel-option">
         <select onChange={handleSelect}>
             <option id="option-content">Select podcast channel</option>
             {showList}

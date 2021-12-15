17cc570e9b4fe5a5cf8d78ea085a534368e0e11f
madgelackie <marionmlackie@gmail.com>
2021-12-15 23:18:08 +0000

    bookmarks saving appropriatley and singlebookmark showing with time feature

--- client/src/components/ChannelInputComponents/AllEpisodeNotes.js
+++ client/src/components/ChannelInputComponents/AllEpisodeNotes.js
@@ -1,39 +1,39 @@
-// import {useState, useEffect} from 'react';
-// import Request from '../../services/helper';
+import {useState, useEffect} from 'react';
+import Request from '../../services/helper';
 
-// const AllEpisodeNotes = ({episodeDBReady, singleBookmark}) => {
+const AllEpisodeNotes = ({episodeDBReady, singleBookmark}) => {
 
-//     const [episodeFromDB, setEpisodeFromDB] = useState(null);
-//     const [epBookmarks, setEpBookmarks] = useState([]);
+    const [episodeFromDB, setEpisodeFromDB] = useState(null);
+    const [epBookmarks, setEpBookmarks] = useState([]);
     
-//     useEffect(() => {
-//         if (episodeDBReady){
-//         const request = new Request();
-//         request.get("/api/episodes/" + episodeDBReady.episodeTitle)
-//         .then((data) => {setEpisodeFromDB(data)})}
-//     }, [episodeDBReady])
+    useEffect(() => {
+        if (episodeDBReady){
+        const request = new Request();
+        request.get("/api/episodes/" + episodeDBReady.episodeTitle)
+        .then((data) => {setEpisodeFromDB(data)})}
+    }, [episodeDBReady])
 
-//     useEffect(() => {
-//         if (episodeDBReady){
-//         const request = new Request();
-//         request.get("/api/episodes/" + episodeDBReady.episodeTitle)
-//         .then((data) => {setEpisodeFromDB(data)})}
-//     }, [singleBookmark])
+    useEffect(() => {
+        if (episodeDBReady){
+        const request = new Request();
+        request.get("/api/episodes/" + episodeDBReady.episodeTitle)
+        .then((data) => {setEpisodeFromDB(data)})}
+    }, [singleBookmark])
 
-//     console.log("What comes from ChannelFeed: ", episodeDBReady)
-//     console.log("This is the returned ep from DB: ", episodeFromDB)
+    console.log("What comes from ChannelFeed: ", episodeDBReady)
+    console.log("This is the returned ep from DB: ", episodeFromDB)
    
-//     const handleClick = () => {
-//         const goToTime = document.getElementById("episode")
-//         goToTime.currentTime = singleBookmark.timestamp
-//     }
+    const handleClick = () => {
+        const goToTime = document.getElementById("episode")
+        goToTime.currentTime = singleBookmark.timestamp
+    }
 
-//     return (
-//         <div class="all-notes">
-//         <button onClick={handleClick}>{singleBookmark.timestamp}</button>
-//         <p>{singleBookmark.note} </p>
-//         </div>
-//     )
-// }
+    return (
+        <div class="all-notes">
+        <button onClick={handleClick}>{singleBookmark.timestamp}</button>
+        <p>{singleBookmark.note} </p>
+        </div>
+    )
+}
 
-// export default AllEpisodeNotes;
+export default AllEpisodeNotes;

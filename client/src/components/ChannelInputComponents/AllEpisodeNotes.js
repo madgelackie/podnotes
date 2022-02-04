f8b1445b4b20fee858f95edcdbd95fa7c75c90f5
madgelackie <marionmlackie@gmail.com>
2022-02-04 21:50:05 +0000

    working on AllNotes box

--- client/src/components/ChannelInputComponents/AllEpisodeNotes.js
+++ client/src/components/ChannelInputComponents/AllEpisodeNotes.js
@@ -20,18 +20,34 @@ const AllEpisodeNotes = ({episodeDBReady, singleBookmark}) => {
         .then((data) => {setEpisodeFromDB(data)})}
     }, [singleBookmark])
 
-    console.log("What comes from ChannelFeed: ", episodeDBReady)
-    console.log("This is the returned ep from DB: ", episodeFromDB)
-   
-    const handleClick = () => {
-        const goToTime = document.getElementById("episode")
-        goToTime.currentTime = singleBookmark.timestamp
-    }
+    // console.log("What comes from ChannelFeed: ", episodeDBReady)
+    // console.log("This is the returned ep from DB: ", episodeFromDB)
 
+   
+// not doing as intended! Currently, allnotes box is rendering but with no notes. Had to put in the if statement as otherwise was getting error in browser as bookmark was empty. But component render isn't happy with calling on a function - would usually just be a variable being passed in.  
+    const makeBookmarkList = (episodeFromDB) => {
+        if (episodeFromDB != null) {
+            episodeFromDB.bookmarks.map((bookmark, index) => {
+            var bookmarkList = <div id="bookmark-items" key={index}>
+            <li value={index}>{bookmark.note}</li>
+            </div>})
+    }
+    return null
+}
+        
+
+    // const handleClick = () => {
+    //     const goToTime = document.getElementById("episode")
+    //     goToTime.currentTime = singleBookmark.timestamp
+    //     console.log("click hit")
+    // }
+    // <button onClick={handleClick}>{singleBookmark.timestamp}</button>
+
+    // button is being used to go back to the timestamp of the displayed bookmark.
     return (
-        <div class="all-notes">
-        <button onClick={handleClick}>{singleBookmark.timestamp}</button>
-        <p>{singleBookmark.note} </p>
+        <div >
+        <p>Bookmarks:</p>
+        {episodeFromDB ? <ul class="all-notes">{bookmarkList} </ul> :null}
         </div>
     )
 }
